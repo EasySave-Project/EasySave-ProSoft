564a692a1ab76da27d18db83f72f2afa8a853807
amdaamouch <amine.daamouch2@soprabanking.com>
2024-01-31 21:50:23 +0100

    feat : Constructeur de la classe BackUpJob

--- EasySave/EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
+++ EasySave/EasySave/obj/Debug/net8.0/EasySave.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2bf14c80483a648d7ea6226beebffe66f8114d29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb276d49c6711b1f1d59203b8d79034beb24ceb1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasySave")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
