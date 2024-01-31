98db7f87764a980fa1712e8ff447436cd3bd0329
amdaamouch <amine.daamouch2@soprabanking.com>
2024-01-31 21:50:23 +0100

    feat : Constructeur de la classe BackUpJob

--- EasySave/EasySave/model/BackupJob.cs
+++ EasySave/EasySave/model/BackupJob.cs
@@ -2,5 +2,23 @@
 {
     public class BackupJob
     {
+        
+        public string sourceDirectory { get; set; }
+        
+        public string targetDirectory { get; set; }
+        
+        public string name { get; set; }
+        
+        public char type { get; set; }
+
+        public BackupJob(String sourceDirectory, String targetDirectory, string name, char type)
+        {
+            this.sourceDirectory = sourceDirectory;
+            this.targetDirectory = targetDirectory;
+            this.name = name;
+            this.type = type;
+        }
+
+
     }
 }
