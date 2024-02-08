88f0bc1b7b5c01dd15fabc6528ad8710958a6b49
Alois-Leroy <alois.leroy.dev@gmail.com>
2024-02-08 18:08:05 +0100

    Gestion Conflits

--- EasySave/EasySave/model/CompleteBackUpJob.cs
+++ EasySave/EasySave/model/CompleteBackUpJob.cs
@@ -1,22 +1,20 @@
 using EasySave.view;
 using EasySave.utils;
-<<<<<<< HEAD
 using System.Threading;
-=======
 using EasySave.services;
->>>>>>> lang
+
 
 namespace EasySave.model
 {
     public class CompleteBackUpJob : BackUpJob
 
     {
-<<<<<<< HEAD
+
         public static long Result { get; internal set; }
 
-=======
+
         private static ConsoleView cv = new ConsoleView();
->>>>>>> lang
+
         public CompleteBackUpJob(string name, string sourceDirectory, string targetDirectory) : base(name, sourceDirectory, targetDirectory)
         {
         }
@@ -27,15 +25,11 @@ namespace EasySave.model
             {
                 long TimeBeforeSave = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
                 FileUtils.CompleteCopyDirectory(sourceDirectory, targetDirectory);
-<<<<<<< HEAD
+
                 long TimeAfterSave = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                Console.WriteLine("Sauvegarde complète réussie.");
+                Console.WriteLine(cv.GetLineLanguage(50));
                 Result = TimeAfterSave - TimeBeforeSave;
                 Console.WriteLine($"Temps d'execution: {Result}");
-=======
-                Console.WriteLine(cv.GetLineLanguage(50));
-                
->>>>>>> lang
             }
             catch (Exception e)
             {
--- EasySave/EasySave/model/DifferentialBackUpJob.cs
+++ EasySave/EasySave/model/DifferentialBackUpJob.cs
@@ -5,12 +5,11 @@ namespace EasySave.model
 {
     public class DifferentialBackUpJob : BackUpJob
     {
-<<<<<<< HEAD
+
         public static long Result { get; internal set; }
 
-=======
         private static ConsoleView cv = new ConsoleView();
->>>>>>> lang
+
         public DifferentialBackUpJob(string name, string sourceDirectory, string targetDirectory) : base(name, sourceDirectory, targetDirectory)
         {
             this.name = name;
@@ -24,15 +23,10 @@ namespace EasySave.model
             {
                 long TimeBeforeSave = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
                 FileUtils.DifferentialCopyDirectory(sourceDirectory, targetDirectory);
-<<<<<<< HEAD
                 long TimeAfterSave = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                Console.WriteLine("Sauvegarde complète réussie.");
                 Result = TimeAfterSave - TimeBeforeSave;
                 Console.WriteLine($"Temps d'execution: {Result}");
-=======
                 Console.WriteLine(cv.GetLineLanguage(53));
-
->>>>>>> lang
             }
             catch (Exception e)
             {
