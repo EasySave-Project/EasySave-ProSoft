83c8a7a1c5a38128f036fba56c1939e9a1a25d12
Alois-Leroy <alois.leroy.dev@gmail.com>
2024-02-15 15:18:44 +0100

    Affichage changement de page OK

---
+++ EasySave/EasySave/view/Home.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace EasySave.view
+{
+    /// <summary>
+    /// Logique d'interaction pour Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        private MainWindow mainWindow;
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Setting setting = new Setting();
+
+            // Accéder à la fenêtre parente et remplacer le contenu par la page2
+            Window parentWindow = Window.GetWindow(this);
+            parentWindow.Content = setting;
+        }
+    }
+}
