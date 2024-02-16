a832e48a1c95ebc371b7c3e4d89c87adc8ee703c
Baptisto06 <baptiste.berretta@viacesi.fr>
2024-02-16 16:13:20 +0100

    code pour afficher Jobs

--- EasySave/EasySave/view/ListJob.xaml.cs
+++ EasySave/EasySave/view/ListJob.xaml.cs
@@ -1,4 +1,8 @@
-﻿using EasySave.view;
+﻿using EasySave.model;
+using EasySave.services;
+using EasySave.controller;
+using EasySave.utils;
+using EasySave.view;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -21,10 +25,109 @@ namespace EasySave.view
     /// </summary>
     public partial class ListJob : Page
     {
+        private int indexPage = 1;
+        private int nbPage = 0;
+
         public ListJob()
         {
             InitializeComponent();
+            ShowListJob();
         }
+
+        //==============================================
+        // Code de la page ListJob
+        //==============================================
+
+        public BackUpController backUpController { get; set; }
+
+        string sCurrentDir = Environment.CurrentDirectory + "\\EasySave\\conf";
+
+        private void ShowListJob()
+        {
+            // Récupération de la liste des jobs
+            List<String> sNameJob = new List<string>();
+            foreach (BackUpJob bj in BackUpManager.listBackUps)
+            {
+                sNameJob.Add(bj.name);
+            }
+
+            // Initialisation des pages
+            nbPage = (int)Math.Ceiling((double)sNameJob.Count / 5);
+            Label_IndexPages.Content = indexPage + " / " + nbPage;
+
+            // Affichage des jobs
+            int startIndex = (indexPage - 1) * 5;
+            for (int i = 0; i < 5; i++)
+            {
+                int index = startIndex + i;
+                if (index < sNameJob.Count)
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            LabelName_Job1.Content = sNameJob[index];
+                            Grid_Job1.Visibility = Visibility.Visible;
+                            Grid_Job1.IsEnabled = true;
+                            break;
+                        case 1:
+                            LabelName_Job2.Content = sNameJob[index];
+                            Grid_Job2.Visibility = Visibility.Visible;
+                            Grid_Job2.IsEnabled = true;
+                            break;
+                        case 2:
+                            LabelName_Job3.Content = sNameJob[index];
+                            Grid_Job3.Visibility = Visibility.Visible;
+                            Grid_Job3.IsEnabled = true;
+                            break;
+                        case 3:
+                            LabelName_Job4.Content = sNameJob[index];
+                            Grid_Job4.Visibility = Visibility.Visible;
+                            Grid_Job4.IsEnabled = true;
+                            break;
+                        case 4:
+                            LabelName_Job5.Content = sNameJob[index];
+                            Grid_Job5.Visibility = Visibility.Visible;
+                            Grid_Job5.IsEnabled = true;
+                            break;
+                    }
+                }
+                else
+                {
+                    switch (i)
+                    {
+                        case 0:
+                            LabelName_Job1.Content = "";
+                            Grid_Job1.Visibility = Visibility.Hidden;
+                            Grid_Job1.IsEnabled = false;
+                            break;
+                        case 1:
+                            LabelName_Job2.Content = "";
+                            Grid_Job2.Visibility = Visibility.Hidden;
+                            Grid_Job2.IsEnabled = false;
+                            break;
+                        case 2:
+                            LabelName_Job3.Content = "";
+                            Grid_Job3.Visibility = Visibility.Hidden;
+                            Grid_Job3.IsEnabled = false;
+                            break;
+                        case 3:
+                            LabelName_Job4.Content = "";
+                            Grid_Job4.Visibility = Visibility.Hidden;
+                            Grid_Job4.IsEnabled = false;
+                            break;
+                        case 4:
+                            LabelName_Job5.Content = "";
+                            Grid_Job5.Visibility = Visibility.Hidden;
+                            Grid_Job5.IsEnabled = false;
+                            break;
+                    }
+                }
+            }
+        }
+
+        //==============================================
+        // Bouton de navigation
+        //==============================================
 
         private void Btn_Home_Click(object sender, RoutedEventArgs e)
         {
@@ -45,6 +148,26 @@ namespace EasySave.view
             Setting setting = new Setting();
             Window parentWindow = Window.GetWindow(this);
             parentWindow.Content = setting;
+        }
+
+        private void Btn_Right_Index_Click(object sender, RoutedEventArgs e)
+        {
+            if (indexPage < nbPage)
+            {
+                indexPage++;
+                Label_IndexPages.Content = indexPage + " / " + nbPage;
+                ShowListJob();
+            }
+        }
+
+        private void Btn_Left_Index_Click(object sender, RoutedEventArgs e)
+        {
+            if (indexPage > 1)
+            {
+                indexPage--;
+                Label_IndexPages.Content = indexPage + " / " + nbPage;
+                ShowListJob();
+            }
         }
     }
 
