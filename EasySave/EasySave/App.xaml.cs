997e64abd299c3600b25454436210b98a4e8f7a3
amdaamouch <amine.daamouch2@soprabanking.com>
2024-02-18 20:48:00 +0100

    modify error message for the backUpManager

--- EasySave/EasySave/App.xaml.cs
+++ EasySave/EasySave/App.xaml.cs
@@ -2,6 +2,7 @@
 using EasySave.utils;
 using System.Configuration;
 using System.Data;
+using System.Diagnostics;
 using System.Windows;
 
 namespace EasySave
@@ -11,18 +12,30 @@ namespace EasySave
     /// </summary>
     public partial class App : System.Windows.Application
     {
-        Settings s = new Settings();
 
-        //App()
-        //{
-        //    System.Threading.Thread.CurrentThread.CurrentUICulture = new System.Globalization.CultureInfo(s.Lang);
-        //}
-        
-        //protected override void OnStartup(StartupEventArgs e)
-        //{
-        //    base.OnStartup(e);
-        //    ManageLang.ChangeLanguage(s.Lang);
-        //}
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            // Obtenez le nom du processus de votre application
+            var currentProcess = Process.GetCurrentProcess();
+            var currentProcessName = currentProcess.ProcessName;
+
+            // Trouvez tous les processus ayant le même nom que votre application
+            var processes = Process.GetProcessesByName(currentProcessName);
+
+            // S'il existe plus d'une instance de ce processus, considérez qu'une instance est déjà en cours d'exécution
+            if (processes.Length > 1)
+            {
+                foreach (var process in processes)
+                {
+                    if (process.Id != currentProcess.Id) // Ignorez le processus actuel
+                    {
+                        // Ici, vous pourriez également implémenter une logique pour terminer les processus enfants si nécessaire
+                        process.Kill(); // Termine le processus
+                    }
+                }
+            }
+        }
     }
-
 }
+
